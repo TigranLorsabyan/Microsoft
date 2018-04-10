fb39a8f9a5e1bf5c2833b7f9df6998ee4991f548
Tigran Lorsabyan <tigrangeo@gmail.com>
2018-04-10 13:23:36 +0400

    Add project files.

---
+++ Cube/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Math;
+
+namespace Cube
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter area");
+            int S = int.Parse(ReadLine());
+            Write("Enter Hight");
+            int H = int.Parse(ReadLine());
+            int V = S * H;
+            WriteLine("Volume = " + V);
+        }
+    }
+}
